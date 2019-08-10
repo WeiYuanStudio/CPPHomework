b6b52acb5607d56f4a43a63ce8099bf47bf1ac20
WeiYuanStudio <weiyuanstudio@gmail.com>
2019-08-10 12:13:36 +0800

    Add Design Pattern Adapter I

---
+++ Design_Pattern/Adapter/I/PrintBanner.java
@@ -0,0 +1,15 @@
+public class PrintBanner extends Banner implements Print {
+    public PrintBanner(String str) {
+    	super(str);
+    }
+
+    @Override
+    public void printWeak() {
+    	super.showWithParen();
+    }
+
+    @Override
+    public void printStrong() {
+	super.showWithAster();
+    }
+}
