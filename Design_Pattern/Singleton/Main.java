e28541706cd20afd8767167a9fb3d3ff2298994b
WeiYuanStudio <weiyuanstudio@gmail.com>
2019-08-22 11:36:21 +0800

    Update Design Pattern Singleton

---
+++ Design_Pattern/Singleton/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        if(singleton1 == singleton2) {
+            System.out.println("singleton1 == singleton2");
+        } else {
+            System.out.println("singleton1 != singleton2");
+        }
+    }
+}
