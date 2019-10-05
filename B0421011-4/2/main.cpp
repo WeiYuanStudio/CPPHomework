b7f0eb86b9e841e4eab8a5720fd0d7e7120f9c42
WeiYuanStudio <weiyuanstudio@gmail.com>
2019-10-05 20:51:03 +0800

    :sparkles: Update Algorithm Course 2 Link List

--- B0421011-4/2/main.cpp
+++ B0421011-4/2/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include "seqList.h"
+#include "linkList.h"
 
 using namespace std;
 
 int main() {
+    cout << "Hello, World!" << endl;
+
+    cout << "Use Seq List: " << endl;
     seqList<int> list(2);
     list.insert(0, 1);
     list.insert(0, 2);
     list.traverse();
+
+    cout << "Use Link List" << endl;
+    linkList<int> listB;
+    listB.insert(0, 1);
+    listB.insert(0, 2);
+    listB.traverse();
 }
